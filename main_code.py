7d0c9056ed4dbc51433b3917472470b9f2065e2a
Sebastian Kinnast <sebastian.kinnast@iu-study.org>
2021-10-08 17:54:16 +0200

    main_code.py modul erzeugt

---
+++ main_code.py
@@ -0,0 +1,32 @@
+"""
+Programm für die Hausarbeit zum Kurs  DLMDWPMP01 - Programmieren mit Python
+
+Autor: Sebastian Kinnast Martikelnr.: 32112741
+
+Tutor: Stephan Fuehrer
+"""
+
+""" Erfoderliche Paket importieren"""
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer
+from sqlalchemy.orm import declarative_base
+import pandas
+import bokeh
+import matplotlib
+
+import database as db
+
+
+db.Datenbank_erzeugen('datenbank_hausarbeit')
+
+
+    
+    
+    
+    
+
+    
+
+    
+    
+    
+
